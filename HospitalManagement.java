806c76b2537490d894b023b178753bd5e420ce1a
DASARI RAMMOHAN <ramdasari160@gmail.com>
2023-08-08 18:38:04 +0530

    Update HospitalManagement.java
    lkjhgf

--- HospitalManagement.java
+++ HospitalManagement.java
@@ -7,7 +7,7 @@ public class HospitalManagement {
 	
 	public  static ArrayList<Patient> patientInfo() {
 		ArrayList<Patient> list=new ArrayList<Patient>();
-		Patient ali=new Patient(10,"alis",25,"heart",9014484452l,3000); 
+		Patient ali=new Patient(10,"alfvis",25,"heart",9014484452l,3000); 
 		Patient ram=new Patient(9,"ram",22,"cough",8790876534l,2000);
 		Patient ravindra=new Patient(60,"ravindra",23,"fever",8886922183l,27999);
 		Patient loki=new Patient(34,"loki",23,"malaria",9347394783l,34520);
