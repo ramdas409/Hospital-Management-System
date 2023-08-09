e92ca8592be4257462617f6d12e43442687fff5b
DASARI RAMMOHAN <ramdasari160@gmail.com>
2023-08-09 12:07:07 +0530

    Update HospitalOptions.java
    vbn

--- HospitalOptions.java
+++ HospitalOptions.java
@@ -14,7 +14,7 @@ boolean b=false;
 for(Patient pp:lis) {
 	if(pp.getId()==p.getId()) {
 		b=true;
-		System.out.println("details already exists");
+		System.out.println("details alrzzeady exists");
 		break;
 	}
 }
