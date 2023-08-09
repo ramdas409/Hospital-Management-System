59dc8799db8c9daa1d0fcee99ccdb2262e61ee71
DASARI RAMMOHAN <ramdasari160@gmail.com>
2023-08-09 14:47:30 +0530

    Update HospitalManagement.java
    vbnmhgre

--- HospitalManagement.java
+++ HospitalManagement.java
@@ -10,7 +10,7 @@ public class HospitalManagement {
 		Patient ali=new Patient(10,"alfvis",25,"heart",9014484452l,30090); 
 		Patient ram=new Patient(19,"radcvm",22,"codfgugh",8790876534l,20000);
 		Patient ravindra=new Patient(60,"ravdhdinsdddra",23,"fever",8886922183l,27999);
-		Patient loki=new Patient(34,"loki",23,"malaria",9347394783l,34520);
+		Patient loki=new Patient(34,"lovbki",23,"malaria",9347394783l,34520);
 		Patient ravi=new Patient(232,"ravi",21,"fever",8142836165l,7362);
 		
 		list.add(ali);
